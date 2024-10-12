e921357d48c4abdc879c7fd4f409f1f6be7561d4
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-13 01:09:35 +0200

    Pridavam uvodni soubory

---
+++ script.js
@@ -0,0 +1,92 @@
+//document.body.innerHTML += "Vítejte na našem webu"
+//document.body.innerHTML += 12 % 5;
+//document.body.innerHTML += (5 * (3 + 7)) % 9;
+//document.body.innerHTML +='Pavel';
+//document.body.innerHTML +='Prací prášek';
+//document.body.innerHTML += "I'm awesome";
+//document.body.innerHTML += 'Řekl: "ahoj"';
+//document.body.innerHTML += '<h1 class="title">Digitální akademie Web</h1<;'
+//document.body.innerHTML += 'Digitální akademie' + ' Web';
+//document.body.innerHTML += 'Digitální akademie' + ' ' + 'Web';
+//document.body.innerHTML += 'Digitální akademie Web' + '';
+//document.body.innerHTML += '<h1>' + 'Digitální akademie Web' + '</h1>';
+
+//document.body.innerHTML += Math.round(3.14);
+//document.body.innerHTML += 70 % Math.round(100 / 3);
+//document.body.innerHTML += Math.floor(3.9);
+//document.body.innerHTML += Math.ceil(3.1);
+//document.body.innerHTML += Math.random();
+//document.body.innerHTML += Math.floor(Math.random() * 10);
+//document.body.innerHTML += 'magdalena' + '.' + 'stranska' + '@mujmail.com';
+
+//const cas = 15;
+//const novyCas = (cas + 17) % 24;
+//document.body.innerHTML += novyCas;
+
+// let sazba = 500;
+// let vyplata = 8 * 21 * sazba;
+// document.body.innerHTML += vyplata;
+
+// sazba = 600;
+// vyplata = 8 * 21 * sazba;
+// document.body.innerHTML += vyplata;
+
+// //sazba = sazba + 100;
+// sazba += 100;
+// vyplata = 8 * 21 * sazba;
+// document.body.innerHTML += vyplata;
+
+// //sazba = sazba * 1.15;
+// sazba *= 1.15;
+// vyplata = 8 * 21 * sazba;
+// document.body.innerHTML += vyplata;
+
+// let email = 'alzbeta.zelena';
+// email += '@gmail.com';
+// document.body.innerHTML += email;
+
+// const cas = 15;
+// const novyCas = (cas + 17) % 24;
+// document.body.innerHTML += novyCas;
+
+// let sazba = 500;
+// let vyplata = 8 * 21 * sazba;
+// document.body.innerHTML += vyplata;
+
+// //sazba = sazba + 100;
+// sazba += 100;
+// vyplata = 8 * 21 * sazba;
+// document.body.innerHTML += vyplata;
+
+// document.body.innerHTML += "<h1>První program</h1>";
+// document.body.innerHTML += '<p>Nový obsah</p>';
+
+//document.body.innerHTML += Math.random()
+
+// const wageInEur = 20;
+// const wageInCzk = wageInEur * 24.55;
+// document.body.innerHTML += '<h1>' + 'Mzda v korunach: ' + wageInCzk + 'Kc' + '</h1>'
+
+// const start = 15;
+// const delka = 25;
+// const konec = (start + delka) % 24;
+// document.body.innerHTML += konec
+
+// const vstupne = 12;
+// const pocetNavstevniku = 174;
+// const pocetPredstaveniZaMesic = 15;
+// const prijem = pocetNavstevniku * vstupne * pocetPredstaveniZaMesic;
+
+
+// document.body.innerHTML += prijem
+// document.body.innerHTML += prijem - (prijem * 0.4 * 0.35)
+
+// const x = 1.725;
+// document.body.innerHTML += Math.round(x);
+
+
+document.body.innerHTML += Math.floor(Math.random() * 6) + 1
+
+
+
+
